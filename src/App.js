b083191688d7352c9bc68127fcbbdf430f88bdf4
uidev-in <mail2loveseries@gmail.com>
2023-11-18 14:07:00 +0530

    added note

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Home from './pages/Home';
 
 function App() {
   return (
+  <>
     <Home/>
+    <p style={{position:"absolute",bottom:"0",color:"#444857"}}>Note: If have missed something, plz let me know. I will do it....</p>
+  </>
+
   );
 }
 
